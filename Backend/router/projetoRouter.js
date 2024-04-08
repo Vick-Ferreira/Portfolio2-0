2daf16fc97f9383a2f175717fb531998b1c9b7f9
Vitoria Ferreira <vitoriaferreirap06@gmail.com>
2024-04-08 17:53:03 -0300

    deploy

--- Backend/router/projetoRouter.js
+++ Backend/router/projetoRouter.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const router = express.Router();
-const projetosController = require('../controller/projetoController');
-const upload = require("../config/multer");
+const router = require('express').Router()
+const Projeto = require('../models/projetos')
+const projetosController = require('../controller/projetoController')
 
-// Rota para criar um novo projeto
+const upload = require("../config/multer")
+
+//crinado rotas para a API
+
 router.post('/', upload.single('video'), projetosController.createProjeto);
 
-// Rota para buscar todos os projetos
-router.get('/', projetosController.buscarProjeto);
+router.get('/', (req, res) => {
+    console.log("Recebida requisição GET para /projeto");
+    // Chame a função buscarProjeto do controller
+    projetosController.buscarProjeto(req, res);
+});
 
 module.exports = router;
