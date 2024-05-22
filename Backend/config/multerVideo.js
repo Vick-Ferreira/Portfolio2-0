7a1a9c5e343436bc033043e1fc642306c3e6518a
Vitoria Ferreira <vitoriaferreirap06@gmail.com>
2024-05-22 13:22:04 -0300

    deploy

--- Backend/config/multerVideo.js
+++ Backend/config/multerVideo.js
@@ -1,9 +1,13 @@
+const multer = require("multer");
+const path = require("path");
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Definindo o diretório de destino dos arquivos
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, uniqueSuffix + '-' + file.originalname); // Salvando apenas o nome do arquivo
-  }
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
 });
+
+module.exports = multer({ storage: storage });
--- Frontend/script.js
+++ Frontend/script.js
@@ -45,7 +45,7 @@ window.addEventListener("scroll", () => {
   }
 });
 function criarImgBtn() {
-  fetch('https://portfolio2-0-g6xxmbq6ra-uw.a.run.app/ImgBtn')
+  fetch('http://localhost:3000/ImgBtn')
     .then(resp => resp.json())
     .then(data => {
       const minhaDiv = document.getElementById("minhaDiv");
@@ -53,7 +53,6 @@ function criarImgBtn() {
         const button = document.createElement("button");
         button.classList.add('btn_modal');
         const imagePath = imgBtn.imagem.replace(/\\/g, '/');
-        const imageUrl = `https://portfolio2-0-g6xxmbq6ra-uw.a.run.app/uploads/imagens/${imagePath}`;
         button.style.backgroundImage = `url(${imagePath})`;
         button.style.backgroundSize = 'contain';
         button.style.backgroundRepeat = 'no-repeat';
@@ -67,7 +66,7 @@ function criarImgBtn() {
     .catch(error => console.error('Erro ao buscar imagens:', error));
 }
 function buscarProjetoPorIndex(index) {// buscar o projeto correspondente com base nesse índice (ONDEM DE ADIÇÃO)
-  fetch('https://portfolio2-0-g6xxmbq6ra-uw.a.run.app/projeto')
+  fetch('http://localhost:3000/projeto')
     .then(resp => resp.json())
     .then(data => {
       const projeto = data[index];
@@ -103,7 +102,7 @@ function enviarFeedback() {//mandando dados form html
     nome: nome,
     opiniao: opiniao
   }
-  fetch('https://portfolio2-0-g6xxmbq6ra-uw.a.run.app/feedback', {
+  fetch('http://localhost:3000/feedback', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
