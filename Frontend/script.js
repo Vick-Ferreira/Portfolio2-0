ea1c3ddf6e7de7e8cb8dd62fe849567535ef52e6
Vitoria Ferreira <vitoriaferreirap06@gmail.com>
2024-05-25 21:01:52 -0300

    update

--- Frontend/script.js
+++ Frontend/script.js
@@ -57,22 +57,17 @@ function verificarVisibilidade(elemento) {
 }
 
 // Função para adicionar ou remover classe quando as habilidades estiverem visíveis ou não
-document.addEventListener('DOMContentLoaded', () => {
+function mostrarHabilidades() {
   const habilidades = document.querySelector('.habilidades');
+  const habilidadesVisiveis = verificarVisibilidade(habilidades);
 
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        habilidades.classList.add('mostrar');
-      } else {
-        habilidades.classList.remove('mostrar');
-      }
-    });
-  }, { threshold: 0.1 });
-
-  observer.observe(habilidades);
-});
-
+  // Verifica se as habilidades estão visíveis e se a classe 'mostrar' já está aplicada
+  if (habilidadesVisiveis && !habilidades.classList.contains('mostrar')) {
+    habilidades.classList.add('mostrar');
+  } else if (!habilidadesVisiveis && habilidades.classList.contains('mostrar')) {
+    habilidades.classList.remove('mostrar');
+  }
+}
 
 // Adiciona o evento de scroll para chamar a função
 window.addEventListener('scroll', mostrarHabilidades);
