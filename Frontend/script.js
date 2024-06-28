1e562ab08ec66aa7cd7612e58f7f03d94dcf2040
Vitoria Ferreira <vitoriaferreirap06@gmail.com>
2024-06-27 21:50:00 -0300

    Upgrade de curriculo + foco freelancer

--- Frontend/script.js
+++ Frontend/script.js
@@ -19,6 +19,31 @@ themeToggle.addEventListener("click", () => {
   }
 });
 
+//animação letras
+//função recursiva
+var textAnimado = document.getElementById("textoAnimado");
+var texto = textAnimado.textContent.trim(); // Obtém o texto do elemento e remove espaços em branco
+
+function animarTexto() {
+  textAnimado.textContent = ""; //limpa o conteudo original
+  //.textContent = obtendo ou definindo o texto dentro desse elemento.
+
+  // Adiciona cada letra, até o ultimo caracter, com um atraso de 100 milissegundos
+  for (var i = 0; i < texto.length; i++) {
+    setTimeout(
+      function (i) {
+        //recuperando a letra do i da interação
+        textAnimado.textContent = textAnimado.textContent + texto[i];
+        if (i == texto.length - 1) {
+          setTimeout(animarTexto, 1000);
+        }
+      },
+      100 * i,
+      i
+    );
+  }
+}
+animarTexto();
 
 //BOTÃO TOPO
 function botaoTopo() {
@@ -34,7 +59,7 @@ function botaoTopo() {
   function scrollToTop() {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
   }
   // Adiciona o evento de rolagem para mostrar ou esconder o botão
@@ -58,95 +83,94 @@ function verificarVisibilidade(elemento) {
 
 // Função para adicionar ou remover classe quando as habilidades estiverem visíveis ou não
 function mostrarHabilidades() {
-  const habilidades = document.querySelector('.habilidades');
+  const habilidades = document.querySelector(".habilidades");
   const habilidadesVisiveis = verificarVisibilidade(habilidades);
 
   // Verifica se as habilidades estão visíveis e se a classe 'mostrar' já está aplicada
-  if (habilidadesVisiveis && !habilidades.classList.contains('mostrar')) {
-    habilidades.classList.add('mostrar');
-  } else if (!habilidadesVisiveis && habilidades.classList.contains('mostrar')) {
-    habilidades.classList.remove('mostrar');
+  if (habilidadesVisiveis && !habilidades.classList.contains("mostrar")) {
+    habilidades.classList.add("mostrar");
+  } else if (
+    !habilidadesVisiveis &&
+    habilidades.classList.contains("mostrar")
+  ) {
+    habilidades.classList.remove("mostrar");
   }
 }
 
 // Adiciona o evento de scroll para chamar a função
-window.addEventListener('scroll', mostrarHabilidades);
+window.addEventListener("scroll", mostrarHabilidades);
 
 // Chama a função inicialmente para verificar a visibilidade
 mostrarHabilidades();
 
-
-
 //ELEMENTOS CRIADOS DINAMICOS COM RECUPERAÇÃO DE DADOS BACKEND.
 function criarImgBtn() {
-  fetch('https://portfolio2-0-k2jz3gicva-uw.a.run.app/files')
-    .then(resp => resp.json())
-    .then(data => {
+  fetch("https://portfolio2-0-k2jz3gicva-uw.a.run.app/files")
+    .then((resp) => resp.json())
+    .then((data) => {
       const minhaDiv = document.getElementById("minhaDiv");
-      minhaDiv.innerHTML = ''; // Limpa a div antes de adicionar novos botões
+      minhaDiv.innerHTML = ""; // Limpa a div antes de adicionar novos botões
       data.forEach((imgBtn, index) => {
         const button = document.createElement("button");
-        button.classList.add('btn_modal');
+        button.classList.add("btn_modal");
 
         const imageUrl = `https://portfolio2-0-k2jz3gicva-uw.a.run.app/files/index/${index}`;
 
         // Criar a imagem
-        const img = document.createElement('img');
+        const img = document.createElement("img");
         img.src = imageUrl;
-        img.alt = 'Imagem do botão'; // Adicione um atributo alt para acessibilidade
+        img.alt = "Imagem do botão"; // Adicione um atributo alt para acessibilidade
 
         // Adicionar a imagem como filho do botão
         button.appendChild(img);
 
         // Adicionar evento de clique ao botão
-        button.addEventListener('click', function () {
+        button.addEventListener("click", function () {
           buscarVideoPorIndex(index);
         });
 
         minhaDiv.appendChild(button);
       });
     })
-    .catch(error => console.error('Erro ao buscar imagens:', error));
+    .catch((error) => console.error("Erro ao buscar imagens:", error));
 }
 
 // Função para buscar vídeo pelo índice
 function buscarVideoPorIndex(index) {
-  fetch('https://portfolio2-0-k2jz3gicva-uw.a.run.app/videos')
-    .then(resp => {
+  fetch("https://portfolio2-0-k2jz3gicva-uw.a.run.app/videos")
+    .then((resp) => {
       if (!resp.ok) {
-        throw new Error('Erro ao buscar vídeos');
+        throw new Error("Erro ao buscar vídeos");
       }
       return resp.json();
     })
-    .then(data => {
+    .then((data) => {
       const video = data[index]; // Busca o vídeo pelo índice
       if (video) {
         exibirDetalhesDoVideo(video, index);
       } else {
-        console.error('Vídeo não encontrado para o índice:', index);
+        console.error("Vídeo não encontrado para o índice:", index);
       }
     })
-    .catch(error => {
-      console.error('Erro ao buscar detalhes do vídeo:', error);
+    .catch((error) => {
+      console.error("Erro ao buscar detalhes do vídeo:", error);
     });
 }
 
-
-
 // IMPORTANTE: vídeo esteja carregado o suficiente para permitir a interação do usuário assim que o modal for aberto
 function exibirDetalhesDoVideo(video, index) {
-  console.log('Exibindo detalhes do vídeo:', video);
-  console.log('Índice do vídeo:', index);
+  console.log("Exibindo detalhes do vídeo:", video);
+  console.log("Índice do vídeo:", index);
 
-  const videoElement = document.createElement('video');
-  videoElement.classList.add('video_Element');
+  const videoElement = document.createElement("video");
+  videoElement.classList.add("video_Element");
 
   const videoUrl = `https://portfolio2-0-k2jz3gicva-uw.a.run.app/videos/index/${index}`;
-  console.log('URL do vídeo:', videoUrl);
+  console.log("URL do vídeo:", videoUrl);
 
   const startTime = performance.now();
 
-  videoElement.addEventListener('loadedmetadata', () => {
+  videoElement.addEventListener("loadedmetadata", () => {
     const endTime = performance.now();
     const loadTime = endTime - startTime;
     console.log(`Tempo de carregamento do vídeo: ${loadTime} ms`);
@@ -158,64 +182,56 @@ function exibirDetalhesDoVideo(video, index) {
   videoElement.muted = true;
   videoElement.playsInline = true;
 
-  const tituloElement = document.createElement('h2');
+  const tituloElement = document.createElement("h2");
   tituloElement.textContent = video.metadata.titulo;
-  tituloElement.classList.add('tituloElement');
+  tituloElement.classList.add("tituloElement");
 
-  const descricaoElement = document.createElement('p');
+  const descricaoElement = document.createElement("p");
   descricaoElement.textContent = video.metadata.descricao;
-  descricaoElement.classList.add('descricaoElement');
+  descricaoElement.classList.add("descricaoElement");
 
-  const modalContent = document.getElementById('modalContent');
-  modalContent.innerHTML = '';
+  const modalContent = document.getElementById("modalContent");
+  modalContent.innerHTML = "";
   modalContent.appendChild(tituloElement);
   modalContent.appendChild(descricaoElement);
   modalContent.appendChild(videoElement);
 
-  const modal = new bootstrap.Modal(document.getElementById('exampleModal'));
+  const modal = new bootstrap.Modal(document.getElementById("exampleModal"));
   modal.show();
 }
-
-
-
-
-
 
 // Chama a função para criar os botões ao carregar a página
 criarImgBtn();
 
-
-function enviarFeedback() {//mandando dados form html
+function enviarFeedback() {
+  //mandando dados form html
   const nome = document.getElementById("nome").value;
   const opiniao = document.getElementById("opiniao").value;
 
-  if (nome === '' || opiniao === '') {
-    alert('Por favor, preencha todos os campos.');
+  if (nome === "" || opiniao === "") {
+    alert("Por favor, preencha todos os campos.");
   } else {
     const feedbackData = {
       nome: nome,
-      opiniao: opiniao
-    }
-    fetch('https://portfolio2-0-k2jz3gicva-uw.a.run.app/feedback', {
-      method: 'POST',
+      opiniao: opiniao,
+    };
+    fetch("https://portfolio2-0-k2jz3gicva-uw.a.run.app/feedback", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(feedbackData)
+      body: JSON.stringify(feedbackData),
     })
-      .then(resp => resp.json())
-      .then(data => {
+      .then((resp) => resp.json())
+      .then((data) => {
         alert("Feedback enviado com sucesso! Obrigada!");
-        document.getElementById("feedbackForm").reset();//Limpar formulario apos evio dos dados
+        document.getElementById("feedbackForm").reset(); //Limpar formulario apos evio dos dados
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("erro ao enviar feedback", error);
-        alert("Ocorreu um erro ao enviar seu feedback. Por favor, tente novamente.");
-      })
+        alert(
+          "Ocorreu um erro ao enviar seu feedback. Por favor, tente novamente."
+        );
+      });
   }
 }
-
-
-
-
-
